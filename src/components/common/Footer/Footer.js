4552ee3bcd709a50ca61c1e71bfe635552843b5a
Rin <ndoraguniru25@gmail.com>
2025-12-17 19:46:12 +0900

    ホームページ修正

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -59,7 +59,10 @@ const Footer = () => {
           <li onClick={() => handleListClick('体験談の投稿')}>体験談を投稿する</li>
           <li onClick={() => handleListClick('みんなの居場所を探す')}>居場所を探す</li>
           <li onClick={() => handleListClick('中学卒業後の進路を探す')}>卒業後の進路を探す</li>
-          <li onClick={() => handleListClick('学校・行政・医療情報の一覧')}>学校・行政・医療情報の一覧</li>
+          <li className={styles.lastRow}>
+            <span onClick={() => handleListClick('学校・行政・医療情報の一覧')}>学校・行政・医療情報の一覧</span>
+            <span className={styles.submitButton} onClick={() => handleListClick('体験談の投稿')}>体験談を投稿する</span>
+          </li>
         </ul>
       </div>
     </footer>
